090bc1a0d5c7ed5d80d1736063c5ee6241284c21
Cyberpunk870 <kg11041963@gmail.com>
2025-11-11 09:17:15 +0530

    Fix routing and file extensions

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   root: '.', // Tell Vite to treat project root as the frontend root
   build: {
     outDir: 'dist', // Output folder
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'index.html') // 👈 Explicitly set input
+      input: resolve(__dirname, 'index.html') // Explicitly set input
     }
   }
-})
+});
