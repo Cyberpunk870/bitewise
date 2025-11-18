6120ee6aa56a9fa1908c9d781da79cdf54100109
Cyberpunk870 <kg11041963@gmail.com>
2025-11-18 07:46:24 +0530

    Defer Firebase init and limit initial Home hydration

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,8 @@
-console.log("VITE_API_BASE=",import.meta.env.VITE_API_BASE);
-// ---- hard-disable Firestore streaming *before* any Firebase import ----
-;(globalThis as any)._FIREBASE_FIRESTORE_FORCE_LONG_POLLING = true;
-;(globalThis as any)._FIREBASE_FIRESTORE_USE_FETCH_STREAMS = false;
-// -----------------------------------------------------------------------
+console.log("VITE_API_BASE=", import.meta.env.VITE_API_BASE);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './index.css';
-import './lib/firebase';
 
-import { getAuth } from "firebase/auth";
-(window as any).getAuth = getAuth;
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router} />
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
